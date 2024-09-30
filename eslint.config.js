7e49bfa1963f99d1f321d65554e5f084636f3369
RodriSLK <sanzrodrigo36@gmail.com>
2024-09-29 22:11:17 -0300

    prueba eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,43 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
+    files: ['*/.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2021, // Puedes ajustar a la versión que estás utilizando
-      sourceType: 'module', // Esta línea es crucial
-      globals: {
-        ...globals.browser,
-        cy: 'readonly',
-        Cypress: 'readonly',
-        test: 'readonly', // Para Jest
-        expect: 'readonly', // Para Jest
+      ecmaVersion: 2021, // Cambiar a 2021 o más reciente
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2021, // Cambiar a 2021 o más reciente
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
       },
     },
-    settings: {
-      react: {
-        version: 'detect', // O especifica la versión exacta
-      },
+    settings: { react: { version: '18.3' } },
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
     },
     plugins: {
-      react: pluginReact,
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...pluginReact.configs.flat.recommended.rules,
-    },
-  },
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      sourceType: 'script',
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
 ];

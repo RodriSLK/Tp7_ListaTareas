bee4abf4203a8903b2c7b177962f8924b9c4fc95
RodriSLK <sanzrodrigo36@gmail.com>
2024-09-30 11:00:16 -0300

    arreglo vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     // Aquí añadimos la configuración de Vitest
     globals: true, // Habilita el uso de funciones como 'test' y 'expect' globalmente
-    environment: 'jsdom', // Simula un navegador para pruebas de componentes Reac
+    environment: 'jsdom', // Simula un navegador para pruebas de componentes React
   },
 });
